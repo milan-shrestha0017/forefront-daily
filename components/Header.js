6b09625c6c7d8bb1a7ff92d1b91ac3399480a134
milan-shrestha0017 <milanshrestha0017@gmail.com>
2021-11-08 23:37:20 -0500

    Added header component

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+
+const Header = () => {
+    return (
+        <Navbar className="text-center" light expand="md">
+            <Container>
+                <NavbarBrand className="logo">ForeFront Daily</NavbarBrand>
+                <h4 className="header-text">Your daily dose of crypto news</h4>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Header;
