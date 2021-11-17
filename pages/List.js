73950b1c84182d92bd0bb430d6842fbf0348252b
milan-shrestha0017 <milanshrestha0017@gmail.com>
2021-11-17 00:19:50 -0500

    Modified codes to use the same image for all curated links

--- pages/List.js
+++ pages/List.js
@@ -1,17 +1,19 @@
 import { Container, Row, Col, Button } from 'reactstrap'
 import Image from 'next/image'
 
+import ffDailyImage from '../public/images/ff-daily.png'
+
 const List = () => {
     const ffList = [
-        { image : "images/ens.png", title : "$ENS Governance Tokens Claim Now Open", link : "https://twitter.com/ensdomains/status/1457862602239926274" },
-        { image : "images/cyber.png", title : "A Declaration of Interdependence of Cyberspace", link : "https://twitter.com/verses_xyz/status/1457872988045336581"},
-        { image : "images/bankless.png", title : "Coinbase Brian Armstrong on Bankless Podcast", link : "https://shows.banklesshq.com/p/-90-brian-armstrong-and-the-future"},
-        { image : "images/bau.png", title : "The BauDAO Building DAO", link : "https://cope.mirror.xyz/OxCfl2K4TJX-LJrkn9FElUFNrXaLiBQOvbZmfp9itZA" },
-        { image : "images/curare.png", title : "A Curation Framework for DAOs", link : "https://jo7.mirror.xyz/ordGOgzoZclKwXN656O6_jC3wXM-FK8A0r8JfcC1d10"},
-        { image : "images/nyc.png", title : "CityCoins coming to NYC", link : "https://twitter.com/mineCityCoins/status/1457728138155229187" },
-        { image : "images/bau.png", title : "The BauDAO Building DAO", link : "https://cope.mirror.xyz/OxCfl2K4TJX-LJrkn9FElUFNrXaLiBQOvbZmfp9itZA" },
-        { image : "images/curare.png", title : "A Curation Framework for DAOs", link : "https://jo7.mirror.xyz/ordGOgzoZclKwXN656O6_jC3wXM-FK8A0r8JfcC1d10"},
-        { image : "images/nyc.png", title : "CityCoins coming to NYC", link : "https://twitter.com/mineCityCoins/status/1457728138155229187" }
+        { title : "$ENS Governance Tokens Claim Now Open", link : "https://twitter.com/ensdomains/status/1457862602239926274" },
+        { title : "A Declaration of Interdependence of Cyberspace", link : "https://twitter.com/verses_xyz/status/1457872988045336581"},
+        { title : "Coinbase Brian Armstrong on Bankless Podcast", link : "https://shows.banklesshq.com/p/-90-brian-armstrong-and-the-future"},
+        { title : "The BauDAO Building DAO", link : "https://cope.mirror.xyz/OxCfl2K4TJX-LJrkn9FElUFNrXaLiBQOvbZmfp9itZA" },
+        { title : "A Curation Framework for DAOs", link : "https://jo7.mirror.xyz/ordGOgzoZclKwXN656O6_jC3wXM-FK8A0r8JfcC1d10"},
+        { title : "CityCoins coming to NYC", link : "https://twitter.com/mineCityCoins/status/1457728138155229187" },
+        { title : "The BauDAO Building DAO", link : "https://cope.mirror.xyz/OxCfl2K4TJX-LJrkn9FElUFNrXaLiBQOvbZmfp9itZA" },
+        { title : "A Curation Framework for DAOs", link : "https://jo7.mirror.xyz/ordGOgzoZclKwXN656O6_jC3wXM-FK8A0r8JfcC1d10"},
+        { title : "CityCoins coming to NYC", link : "https://twitter.com/mineCityCoins/status/1457728138155229187" }
     ] 
 
     return (
@@ -20,7 +22,7 @@ const List = () => {
                 <Row className="justify-content-center">
                     <Col md={8}>
                         <div className="text-center">
-                            <h4 className="list-title font-weight-normal text-dark">
+                            <h4 className="list-title font-weight-normal">
                                 Some of the best bits of news and articles we picked for you
                             </h4>
                         </div>
@@ -32,9 +34,9 @@ const List = () => {
                             <Col key={key} md={4}>
                                 <div className="post-box">
                                     <div className="mb-3">
-                                        <Image src={"/" + list.image} width={300} height={200} alt="" />
+                                        <Image src={ffDailyImage} width={175} height={175} alt="FF Daily Logo" />
                                     </div>
-                                    <h5 className="text-dark font-weight-bold pt-1 mb-3">{list.title}</h5>
+                                    <h5 className="link-title font-weight-bold pt-1 mb-3">{list.title}</h5>
                                     <a href={list.link} target="_blank" rel="noopener noreferrer"><Button color="success" block>Read article</Button></a>
                                 </div>
                             </Col>
